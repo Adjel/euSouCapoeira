196a71a7a40bb183516485a4d76f017c82f6dfc9
Adjel <adjel.touaoula@gmail.com>
2024-06-14 17:18:05 +0200

    search icon

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { GoSearch } from "react-icons/go";
+
+function SearchBar() {
+  return (
+    <>
+      <GoSearch />
+    </>
+  );
+}
+
+export default SearchBar;
