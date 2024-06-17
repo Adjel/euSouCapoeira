5b13f3ddd8d7e843f64668ab6134342868d96b4d
Adjel <adjel.touaoula@gmail.com>
2024-06-17 21:30:09 +0200

    feat(basketButton): ui

--- src/components/BasketButton/BasketButton.js
+++ src/components/BasketButton/BasketButton.js
@@ -1,24 +1,29 @@
 "use client";
 import React, { useState } from "react";
-import styles from "../UserButtonsComponent/userButtonsComponent.module.css";
-import nEBasket from "../../../public/nEBasket.svg";
-import basket from "./public/basket.svg";
 import Image from "next/image";
+import nebasket from "../../../public/nEBasket.svg";
+import basketIcon from "../../../public/basket.svg";
 
 function BasketButton() {
   const [basket, setBasket] = useState([]);
 
   return (
-    <>
+    <div className="cursor-pointer">
       {basket.length > 0 ? (
         <>
-          <Image src={nEBasket} className={styles.userNavItem} />
-          <span className="absolute right-0.5 -top-1 w-3 h-1">3</span>
+          <Image src={nebasket} className="ml-4 w-8 h-8" />
+          <span
+            className={`absolute text-xs top-1.5 ${
+              basket.length < 10 ? "right-2.5" : "right-1.5"
+            }`}
+          >
+            {basket.length}
+          </span>
         </>
       ) : (
-        <Image src={basket} className={styles.userNavItem} />
+        <Image src={basketIcon} className="ml-4 w-8 h-8" />
       )}
-    </>
+    </div>
   );
 }
 
