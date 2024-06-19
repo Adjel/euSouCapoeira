c9a7cbd368d8402471fda9f42cb1c1a09d529283
Adjel <adjel.touaoula@gmail.com>
2024-06-19 18:53:25 +0200

    component file

---
+++ src/components/homePage/homeHeader/homeHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function homeHeader() {
+  return <div></div>;
+}
+
+export default homeHeader;
---
+++ src/components/homePage/homeHeader/index.js
@@ -0,0 +1,2 @@
+export * from './homeHeader';
+export { default } from './homeHeader';
