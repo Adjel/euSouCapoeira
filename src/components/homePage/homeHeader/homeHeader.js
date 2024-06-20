2bb1b9d81e397f5076ee45ef335629eec10a5804
Adjel <adjel.touaoula@gmail.com>
2024-06-20 13:51:25 +0200

    try fixing

--- src/components/homePage/homeHeader/homeHeader.js
+++ src/components/homePage/homeHeader/homeHeader.js
@@ -1,4 +1,11 @@
-import React from 'react';
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 function homeHeader() {
   return <div></div>;
