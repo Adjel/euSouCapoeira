a0a70ed6599964dca7b86dc07b8885cd67ff0498
Adjel <adjel.touaoula@gmail.com>
2024-06-21 16:59:13 +0200

    feat(homeHeader): categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -7,12 +7,14 @@ export default function Categories() {
       <h1 className="text-2xl font-bold mb-2 text-center first-letter:uppercase">
         Nos Catégories
       </h1>
-      {categories.map(({ title, image, alt }) => (
-        <>
-          <CategoryItem title={title} image={image} alt={alt} />
-          <div className="h-0.1 bg-color-divider" />
-        </>
-      ))}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {categories.map(({ title, image, alt }) => (
+          <div className="felx">
+            <CategoryItem title={title} image={image} alt={alt} />
+            <div className="h-0.1 bg-color-divider" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
