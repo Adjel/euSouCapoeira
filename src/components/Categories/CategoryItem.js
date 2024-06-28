219f038297f7a81aa8ff670d9d2086b0343381e6
Adjel <adjel.touaoula@gmail.com>
2024-06-28 18:13:46 +0200

    first commit

--- src/components/Categories/CategoryItem.js
+++ src/components/Categories/CategoryItem.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 function CategoryItem({ title, image, alt }) {
+  const getLink = () => {
+    if (title === "vêtements") return "/vetements";
+    else {
+      throw new Error("You need to pass a valid category");
+    }
+  };
+
   return (
     <div className="flex items-center p-2 pr-3">
       <Image className="size-24 pr-4" src={image} alt={alt} />
-      <h2 className="first-letter:uppercase">{title}</h2>
+      <Link href={getLink()}>
+        <h2 className="first-letter:uppercase">{title}</h2>
+      </Link>
     </div>
   );
 }
