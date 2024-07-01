9e15c4045d8375e4a03c8459bdce0a9b13fef4aa
Adjel <adjel.touaoula@gmail.com>
2024-07-01 17:40:07 +0200

    shad Form

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useLoginModalStore, useUserStore } from "@/stores/useUserStore";
 import { IoCloseOutline } from "react-icons/io5";
 import DashboardNav from "../DashboardNav";
+import SignUpForm from "../SignUpForm";
 
 function LoginModal() {
   const { isOpen } = useLoginModalStore();
@@ -13,16 +14,19 @@ function LoginModal() {
       <div>
         <div className="z-50 absolute top-0 bottom-0 w-full h-full bg-extreme-dark-gray opacity-50"></div>
         <aside className="flex flex-col gap-36 z-50 absolute top-0 bottom-0 right-0 w-full md:w-96 lg:w-31.5 h-full p-8 bg-white">
+          <IoCloseOutline className="absolute size-12 mr-16 place-self-end" />
           {user ? (
             <>
-              <IoCloseOutline className="absolute size-12 mr-16 place-self-end" />
               <h2 className="w-fit text-3xl font-bold m-0 p-0">
                 Espace client
               </h2>
               <DashboardNav />
             </>
           ) : (
-            <></>
+            <>
+              <h2 className="w-fit text-3xl font-bold m-0 p-0">Connexion</h2>
+              <SignUpForm />
+            </>
           )}
         </aside>
       </div>
