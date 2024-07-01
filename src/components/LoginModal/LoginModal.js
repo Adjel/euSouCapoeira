e3170a2051d3d60e5b8414f5258fc4e915602c16
Adjel <adjel.touaoula@gmail.com>
2024-07-01 16:37:16 +0200

    background behing modal

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -8,9 +8,8 @@ function LoginModal() {
   return (
     isOpen && (
       <div>
-        <div className="bg-black">
-          <aside className="z-50 absolute top-0 bottom-0 right-0 w-full md:w-96 lg:w-31.5 h-full bg-white"></aside>
-        </div>
+        <div className="z-50 absolute top-0 bottom-0 w-full h-full bg-extreme-dark-gray opacity-50"></div>
+        <aside className="z-50 absolute top-0 bottom-0 right-0 w-full md:w-96 lg:w-31.5 h-full bg-white"></aside>
       </div>
     )
   );
