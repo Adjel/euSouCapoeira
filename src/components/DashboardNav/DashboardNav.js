4ddd25a9c2a8b050a2d3c08796b751fb16ab2895
Adjel <adjel.touaoula@gmail.com>
2024-07-02 19:18:19 +0200

    feat(dashboard): base ui

--- src/components/DashboardNav/DashboardNav.js
+++ src/components/DashboardNav/DashboardNav.js
@@ -2,15 +2,21 @@ import React from "react";
 import Link from "next/link";
 import { useUserStore } from "@/stores/useUserStore";
 
-function DashboardNav() {
+function DashboardNav({ isModal = true }) {
   const { clearUser } = useUserStore();
 
-  const linkStyle = "w-fit text-xl font-bold m-0 p-0";
   const liStyle = "cursor-pointer hover:text-color-gold";
+  const linkStyle = `w-fit ${
+    isModal ? "text-xl" : "text-lg"
+  } font-bold m-0 p-0`;
   const textStyle = "first-letter:uppercase";
 
   return (
-    <ul className="flex flex-col h-full justify-center gap-16">
+    <ul
+      className={`flex flex-col h-full justify-center ${
+        isModal ? "gap-16" : "gap-4"
+      }`}
+    >
       <li className={liStyle}>
         <Link href="/dashboard" className={`${linkStyle}`}>
           <div className="textStyle">Informations</div>
