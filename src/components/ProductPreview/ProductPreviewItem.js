3cb74f0febf2d0099e14567a2486738e77557219
Adjel <adjel.touaoula@gmail.com>
2024-07-02 18:38:16 +0200

    fix(productPreviewItem):fix height to get fixed size

--- src/components/ProductPreview/ProductPreviewItem.js
+++ src/components/ProductPreview/ProductPreviewItem.js
@@ -4,9 +4,9 @@ import RatingComponent from "../RatingComponent";
 
 function ProductPreviewItem({ productName, image, alt, price, rate, rateNbr }) {
   return (
-    <div className="w-fit h-auto flex flex-col justify-center">
+    <div className="w-auto h-auto flex flex-col justify-center">
       <Image
-        className="w-fit h-auto pr-4 border-2 border-color-text-medium-gray"
+        className="w-64 h-64 pr-4 border-2 border-color-text-medium-gray overflow-hidden"
         src={image}
         alt={alt}
       />
