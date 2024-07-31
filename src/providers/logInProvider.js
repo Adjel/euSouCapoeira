72c3e9ce3ddfebac79d481d1ad559d2965dc197a
Adjel <adjel.touaoula@gmail.com>
2024-07-31 18:19:43 +0200

    using local storage in useUserStore

--- src/providers/logInProvider.js
+++ src/providers/logInProvider.js
@@ -1,51 +1,8 @@
 const mockUserToken = async ({ email, password }) => {
   // have to await here
   // get user from API
+  const user = {};
+  return user;
 };
-
-// Have to be deleted because it's a mock
-export async function mockUser() {
-  const currentDate = new Date();
-
-  const tomorrow = new Date(currentDate);
-  const yesterday = new Date(currentDate - 1);
-  tomorrow.setDate(currentDate.getDate() + 1);
-  return {
-    business: "",
-    firstName: "Patrick",
-    lastName: "toupie",
-    email: "P-toupie@toto.com",
-    addresses: [
-      {
-        // TODO: business have to be here
-        date: yesterday,
-        isCurrent: false,
-        firstName: "Patrick",
-        lastName: "toupie",
-        street: "10 rue du vent",
-        zipCode: "41000",
-        city: "Dally",
-        country: "Thailand",
-      },
-      {
-        date: tomorrow,
-        isCurrent: true,
-        firstName: "Jean",
-        lastName: "toupie",
-        street: "12 rue de la montagne",
-        zipCode: "70000",
-        city: "David",
-        country: "Panama",
-      },
-    ],
-    commands: [
-      {
-        date: "",
-        articles: [],
-        totalPrice: "",
-      },
-    ],
-  };
-}
 
 export default mockUserToken;
--- src/stores/useUserStore.js
+++ src/stores/useUserStore.js
@@ -1,106 +1,117 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import createAccount, { mockUser } from "@/providers/logInProvider";
 import { toast } from "@/components/ui/use-toast";
 
-const useUserStore = create((set) => ({
-  user: null,
-  createUser: (values) => {
-    console.log(values);
-    const {
-      business,
-      email,
-      password,
-      firstName,
-      lastName,
-      city,
-      street,
-      zipCode,
-      country,
-    } = values;
-    const user = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password,
-      addresses: [
-        {
+// to prevent access storage from server side
+const isClient = typeof window !== "undefined";
+
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      createUser: (values) => {
+        const {
+          business,
+          email,
+          password,
           firstName,
           lastName,
-          date: new Date(),
-          isCurrent: true,
-          business: business,
-          street,
           city,
+          street,
           zipCode,
           country,
-        },
-      ],
-    };
-    set({ user });
-    // TODO: get response from API
-    if ("mock" === "mock")
-      toast({
-        title: "Votre compte à bien été crée",
-        description: "Vous êtes connecté",
-      });
-    return true;
-  },
-  updateUser: (updatedUser) => {
-    set((state) => ({
-      user: { ...state.user, ...updatedUser },
-    }));
-    //TODO: Return success from API
-    return true;
-  },
-  clearUser: () => set({ user: null }),
-  addAdress(address) {
-    const date = new Date();
-    const newAddress = {
-      date,
-      ...address,
-      // The site only support France shipment, so we secure the country
-      country: "France",
-    };
-    set((state) => ({
-      user: {
-        ...state.user,
-        addresses: [...state.user.addresses, newAddress],
+        } = values;
+        const user = {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+          addresses: [
+            {
+              firstName,
+              lastName,
+              date: new Date(),
+              isCurrent: true,
+              business: business,
+              street,
+              city,
+              zipCode,
+              country,
+            },
+          ],
+        };
+        set({ user });
+        // TODO: get response from API
+        if ("mock" === "mock")
+          toast({
+            title: "Votre compte à bien été crée",
+            description: "Vous êtes connecté",
+          });
+        return true;
       },
-    }));
-    toast({
-      title: "Votre adresse à bien été enregistrée",
-    });
-    //TODO: Return success from API
-    return true;
-  },
+      updateUser: (updatedUser) => {
+        set((state) => ({
+          user: { ...state.user, ...updatedUser },
+        }));
+        //TODO: Return success from API
+        return true;
+      },
+      clearUser: () => set({ user: null }),
+      addAdress(address) {
+        const date = new Date();
+        const newAddress = {
+          date,
+          ...address,
+          // The site only support France shipment, so we secure the country
+          country: "France",
+        };
+        set((state) => ({
+          user: {
+            ...state.user,
+            addresses: [...state.user.addresses, newAddress],
+          },
+        }));
+        toast({
+          title: "Votre adresse à bien été enregistrée",
+        });
+        //TODO: Return success from API
+        return true;
+      },
 
-  setCurrentAddress: (date) =>
-    set((state) => {
-      const updatedAddresses = state.user.addresses.map((address) => ({
-        ...address,
-        isCurrent: address.date === date,
-      }));
-      return {
-        user: {
-          ...state.user,
-          addresses: updatedAddresses,
-        },
-      };
-    }),
+      setCurrentAddress: (date) =>
+        set((state) => {
+          const updatedAddresses = state.user.addresses.map((address) => ({
+            ...address,
+            isCurrent: address.date === date,
+          }));
+          return {
+            user: {
+              ...state.user,
+              addresses: updatedAddresses,
+            },
+          };
+        }),
 
-  deleteAddress: (date) =>
-    set((state) => {
-      const addresses = state.user.addresses.filter(
-        (address) => address.date !== date
-      );
-      return {
-        user: {
-          ...state.user,
-          addresses,
-        },
-      };
+      deleteAddress: (date) =>
+        set((state) => {
+          const addresses = state.user.addresses.filter(
+            (address) => address.date !== date
+          );
+          return {
+            user: {
+              ...state.user,
+              addresses,
+            },
+          };
+        }),
     }),
-}));
+    {
+      name: "user-storage",
+      getStorage: () => (isClient ? localStorage : null),
+    }
+  )
+);
 
 const useSignIn = create((set) => ({
   signIn: async (email, password) => {
