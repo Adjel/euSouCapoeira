16d05ddfab6aa816751cba08c24220645452502c
Adjel <adjel.touaoula@gmail.com>
2024-08-04 15:52:09 +0200

    feat(user): user in coockies

---
+++ src/coockieStore/userCoockies.js
@@ -0,0 +1,32 @@
+import Coockies from "js-cookie";
+
+export const setUserCookies = (user) => {
+  console.log(user);
+  try {
+    Coockies.set("user", JSON.stringify(user), { expires: 7 });
+  } catch (e) {
+    // Crashlytics
+    //throw new Error(`Impossible de sauvegarder l'utilisateur dans les cookies: ${e}`);
+  }
+};
+
+export const getUserFromCookies = () => {
+  try {
+    const cookie = Coockies.get("user");
+    console.log(cookie);
+    if (!cookie) {
+      // Crashlytics
+      //throw new Error(`L'utilisateur n'existe pas dans les cookies: ${e}`);
+    }
+    const test = JSON.parse(cookie);
+    console.log(test);
+    return JSON.parse(cookie);
+  } catch (e) {
+    // crashlitycs
+    //console.log(new Error(`Impossible car l'utilisateur n'existe pas: ${e}`));
+  }
+};
+
+export const removeUserCoockie = () => {
+  Coockies.remove("user");
+};
