bbfcef650610a954bb0cf3677d7f003617a53591
Adjel <adjel.touaoula@gmail.com>
2024-08-27 12:52:07 +0200

    forget console.log

--- src/components/BreadcrumbLinkComponent/BreadcrumbLinkComponent.js
+++ src/components/BreadcrumbLinkComponent/BreadcrumbLinkComponent.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React from "react";
 
 function BreadcrumbLinkComponent({ className, link = "", children }) {
-  console.log(link);
   return (
     <Link className={`text-base ${className}`} href={`/${link}`}>
       {children}
