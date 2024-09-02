48ec0085fc2c1d5a86fb27f228221eb75e88a236
Adjel <adjel.touaoula@gmail.com>
2024-09-02 12:03:34 +0200

    feat(recommand): add products to the list

--- src/providers/RecomandsProvider.js
+++ src/providers/RecomandsProvider.js
@@ -2,6 +2,11 @@ import clothes from "../../public/fakeCatsImages/clothes.jpg";
 import music from "../../public/fakeCatsImages/music.jpg";
 import entrain from "../../public/fakeCatsImages/entrain.jpg";
 import livretexpressao from "../../public/fakeApi/livretexpressao.jpg";
+import tshirt_femme_capoeira_carybe from "../../public/fakeApi/vetements/tshirt/tshirt_femme_capoeira_carybe.jpeg";
+import tshirt_fb_femme_capoeira_carybe from "../../public/fakeApi/vetements/tshirt/tshirt_fb_femme_capoeira_carybe.jpeg";
+import pantalon_capoeira_noir_jamaique_face from "../../public/fakeApi/vetements/pantalon/pantalon_capoeira_noir_jamaique_face.jpeg";
+import abada_capoeira_blanc_jamaica_side from "../../public/fakeApi/vetements/pantalon/abada_capoeira_blanc_jamaica_side.jpeg";
+import abada_capoeira_blanc_jamaica from "../../public/fakeApi/vetements/pantalon/abada_capoeira_blanc_jamaica.jpeg";
 
 export const products = [
   {
@@ -21,6 +26,39 @@ export const products = [
     variants: [],
 
     images: [{ image: entrain, alt: "un sac de frappe" }],
+  },
+  {
+    name: "ABADA DE CAPOEIRA BLANC - JAMAICA",
+    price: 47.94,
+    availability: "now",
+    id: "304a",
+    date: new Date().toISOString(),
+    isBestSeller: false,
+    specs: [
+      "Marque : Capoeira Casual",
+      "Matériau : Coton",
+      "Couleur : Beige",
+      "Tailles : M, L, XL",
+      "Ceinture : Élastique avec cordon",
+    ],
+    sizes: [],
+    variants: [
+      {
+        id: "304b",
+        image: pantalon_capoeira_noir_jamaique_face,
+        alt: "un abada noir de style jamaica",
+      },
+    ],
+    images: [
+      {
+        image: abada_capoeira_blanc_jamaica,
+        alt: "Abada blanc jamaica de face",
+      },
+      {
+        image: abada_capoeira_blanc_jamaica_side,
+        alt: "Abada blanc jamaica de coté",
+      },
+    ],
   },
   {
     name: "Livre Expressao - Édition Spéciale",
@@ -60,6 +98,39 @@ export const products = [
     images: [
       { image: clothes, alt: "Chaussure Capoeira bleu-gris vue 1" },
       { image: clothes, alt: "Chaussure Capoeira bleu-gris vue 2" },
+    ],
+  },
+  {
+    name: "TSHIRT FEMME CAPOEIRA - CARYBÉ BLEU",
+    price: 28.0,
+    availability: "now",
+    id: "204a",
+    date: new Date().toISOString(),
+    isBestSeller: false,
+    specs: [
+      "Marque : Capoeira Style",
+      "Matériau : 100% coton",
+      "Couleur : Rouge",
+      "Tailles : S, M, L, XL",
+      "Impression : Graphique",
+    ],
+    sizes: [],
+    variants: [
+      {
+        id: "204b",
+        image: tshirt_fb_femme_capoeira_carybe,
+        alt: "photo du t shirt d'une autre couleur",
+      },
+    ],
+    images: [
+      {
+        image: tshirt_fb_femme_capoeira_carybe,
+        alt: "T-shirt femme CARYBÉ porté sur fond blanc",
+      },
+      {
+        image: tshirt_femme_capoeira_carybe,
+        alt: "T-shirt femme CARYBÉ porté par un manequin",
+      },
     ],
   },
   {
